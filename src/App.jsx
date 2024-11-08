de56f8de2bad8e43461621750dd915aee203528c
Kavinda Dewmith <kavindadewmith@gmail.com>
2024-11-08 14:17:53 +0530

    All files added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+
+import React from "react";
+import AvatarModel from "./components/AvatarModel";
+
+const App = () => {
+  return (
+    <div>
+      <h1>3D Model using Three.js</h1>
+      <AvatarModel />
+    </div>
+  );
+};
+
+export default App;
